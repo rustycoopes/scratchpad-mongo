c2f567a2a87a7c698e7d216d0f3ae91f84ee4b8f
rustycoopes <russell.cooper@btinternet.com>
2021-01-05 16:25:11 -0500

    original commit

---
+++ kingmong.py
@@ -0,0 +1,12 @@
+
+
+def loaddata():
+    # TODO : connects to mongo and loads some data
+    # console out some strucute.
+    return None
+
+
+def savedata():
+    # TODO : connects to mongo and saves a json document - somethign based on time
+    # with an index.
+    return None
